374d685e2bae4a55bbd8230947cbed0664b9f2ca
DaffaAzzikri <daffaazzikrii@gmail.com>
2025-10-29 22:11:35 +0800

    Add files via upload

---
+++ Modul 4/soal2/Anjing.java
@@ -0,0 +1,24 @@
+package praktikum4.soal2;
+
+public class Anjing extends HewanPeliharaan {
+
+    private String warnaBulu;
+    private String[] kemampuan;
+
+    public Anjing(String n, String r, String w, String[] k) {
+
+        super(r, n);
+        this.warnaBulu = w;
+        this.kemampuan = k;
+    }
+
+    public void displayDetailAnjing() {
+        super.display();
+
+        System.out.println("Memiliki warna bulu : " + this.warnaBulu);
+        String kemampuanStr = String.join(" ", this.kemampuan);
+        System.out.println("Memiliki kemampuan : " + kemampuanStr);
+    }
+}
+
+
